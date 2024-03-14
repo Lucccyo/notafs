9db87602ad7e24d96525d6c67579a51224eb4915
Lucccyo <cha.git@mailo.fr>
2024-03-14 15:41:55 +0100

    add verbose tests

--- test_ortac.sh
+++ test_ortac.sh
@@ -1,4 +1,4 @@
 ortac qcheck-stm tests/gospel/notafs_kv.mli "make ()" "t" > tests/gospel/notafs_ortac.ml
 sed -ri 's/let\s\_/let main \(\)/g' tests/gospel/notafs_ortac.ml
 sed -ri 's/\(string, v option\) \(->\)/(string -> v option)/g' tests/gospel/notafs_ortac.ml
-dune exec -- tests/gospel/test_kv.exe
+dune exec -- tests/gospel/test_kv.exe --verbose
